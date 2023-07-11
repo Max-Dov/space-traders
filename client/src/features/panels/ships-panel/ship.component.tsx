487ecfffdedb046aa5eb2b11d476e38299f1f2ea
Max-Dov <maksimdovydenko@protonmail.com>
2023-07-11 19:23:28 +0200

    ships-panel: refine ship info design

--- client/src/features/panels/ships-panel/ship.component.tsx
+++ client/src/features/panels/ships-panel/ship.component.tsx
@@ -21,59 +21,87 @@ export const Ship = ({ ship }: ShipProps) => {
     cargo
   } = ship;
   return <section className="ship">
+
+    {/** Header with ship name, class, location */}
+    <div className="flex-row header-info">
+      <span className="ship-name">{registration.name}</span>
+      {' '}
+      <span className="role">({registration.role.toLowerCase()}</span>
+      {' '}
+      <span className="frame">{frame.name.split(' ')[1]})</span>
+      {' '}
+      <Icon name="ArrowRight" />
+      {' '}
+      <span className="ship-location">{nav.waypointSymbol}</span>
+    </div>
+
     <div className="flex-row">
       <img className="frame-image" alt={frame.name} src={`/${frame.symbol.toLowerCase()}.webp`} />
       <div>
-        <div className="ship-name">{registration.name}</div>
-        <div>
-          <span className="role">{registration.role.toLowerCase()}</span>
-          {' '}
-          <span className="frame">{frame.name.split(' ')[1]}</span>
+
+        <div className="info-bit">
+          <div className="info-bit-header">
+            Status
+          </div>
+          <div className="info-bit-content">
+            <div className="flex-row">
+              <span className={classNames({ 'dimmed': nav.status !== 'IN_TRANSIT' })}>In Transit</span>
+              /
+              <span className={classNames({ 'dimmed': nav.status !== 'IN_ORBIT' })}>In Orbit</span>
+              /
+              <span className={classNames({ 'dimmed': nav.status !== 'DOCKED' })}>Docked</span>
+            </div>
+          </div>
         </div>
-        <div>
-          <span className="ship-location">{nav.waypointSymbol}</span>
+
+        <div className="info-bit">
+          <div className="info-bit-header">
+            Engines
+          </div>
+          <div className="info-bit-content">
+            <div className="flex-row">
+              <span className={classNames({ 'dimmed': nav.flightMode !== 'CRUISE' })}>Cruise</span>
+              /
+              <span className={classNames({ 'dimmed': nav.flightMode !== 'DRIFT' })}>Drift</span>
+              /
+              <span className={classNames({ 'dimmed': nav.flightMode !== 'STEALTH' })}>Stealth</span>
+              /
+              <span className={classNames({ 'dimmed': nav.flightMode !== 'BURN' })}>Burn</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="ship-stats">
-      <div className="info-bit">
-        <div className="info-bit-header">
-          <Tooltip tooltipText="Fuel" isIconTooltip customIcon={<Icon name="GasCan" />} />
-        </div>
-        <div className="info-bit-content">
-          {fuel.current} / {fuel.capacity}
-        </div>
-      </div>
-      <div className="info-bit">
-        <div className="info-bit-header">
-          Status
-        </div>
-        <div className="info-bit-content">
-          {nav.status}
-        </div>
-      </div>
-      <div className="info-bit">
-        <div className="info-bit-header">
-          Flight Mode
-        </div>
-        <div className="info-bit-content">
-          {nav.flightMode}
-        </div>
-      </div>
-      <div className="info-bit">
-        <div className="info-bit-header">
-          Crew
-        </div>
-        <div className="info-bit-content">
-          {crew.current}
-        </div>
-      </div>
-      <div className="info-bit">
-        <div className="info-bit-header">
-          Cargo
-        </div>
-        <div className="info-bit-content">
-          {cargo.units} / {cargo.capacity}
+
+        <div className="flex-row">
+
+          <div className="info-bit">
+            <div className="info-bit-header column-header">
+              <Tooltip tooltipText="Fuel" isIconTooltip customIcon={<Icon name="GasCan" />} />
+            </div>
+            <div className="info-bit-content">
+              {fuel.current} / {fuel.capacity}
+            </div>
+          </div>
+
+          <div className="info-bit">
+            <div className="info-bit-header column-header">
+              Cargo
+            </div>
+            <div className="info-bit-content">
+              {cargo.units} / {cargo.capacity}
+            </div>
+          </div>
+
+          <div className="info-bit">
+            <div className="info-bit-header column-header">
+              Crew
+            </div>
+            <div className="info-bit-content">
+              <span className="dimmed">[{crew.required} - </span>
+              {crew.current}
+              <span className="dimmed"> - {crew.capacity}]</span>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
