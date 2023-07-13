433d77815407721ddef4af9ee68dbcc5457826b1
Max-Dov <maksimdovydenko@protonmail.com>
2023-07-13 20:48:00 +0200

    ships-panel-tooltips: add tooltips to ships panel

--- client/src/features/panels/ships-panel/ship.component.tsx
+++ client/src/features/panels/ships-panel/ship.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Ship as ShipInterface } from '@types';
 import './ship.styles.scss';
 import classNames from 'classnames';
-import { Icon } from '@shared';
+import { Icon, Tooltip } from '@shared';
 
 interface ShipProps {
   ship: ShipInterface;
@@ -28,7 +28,10 @@ export const Ship = ({ ship }: ShipProps) => {
       <span>
         <span className="role">({registration.role.toLowerCase()}</span>
         {' '}
-        <span className="frame">{frame.name.split(' ')[1]})</span>
+        <span className="frame">
+          <Tooltip tooltipText={frame.description}>{frame.name.split(' ')[1]}</Tooltip>
+          )
+        </span>
       </span>
       <Icon name="ArrowRight" />
       <span className="ship-location">{nav.waypointSymbol}</span>
@@ -43,30 +46,26 @@ export const Ship = ({ ship }: ShipProps) => {
             Status
           </div>
           <div className="info-bit-content">
-            <div className="flex-row">
-              <span className={classNames({ 'dimmed': nav.status !== 'IN_TRANSIT' })}>In Transit</span>
-              /
-              <span className={classNames({ 'dimmed': nav.status !== 'IN_ORBIT' })}>In Orbit</span>
-              /
-              <span className={classNames({ 'dimmed': nav.status !== 'DOCKED' })}>Docked</span>
-            </div>
+            <span className={classNames({ 'dimmed': nav.status !== 'IN_TRANSIT' })}>In Transit</span>
+            {' / '}
+            <span className={classNames({ 'dimmed': nav.status !== 'IN_ORBIT' })}>In Orbit</span>
+            {' / '}
+            <span className={classNames({ 'dimmed': nav.status !== 'DOCKED' })}>Docked</span>
           </div>
         </div>
 
         <div className="info-bit">
           <div className="info-bit-header">
-            Engines
+            <Tooltip tooltipText="The ship's set speed when traveling between waypoints or systems.">Engines</Tooltip>
           </div>
           <div className="info-bit-content">
-            <div className="flex-row">
-              <span className={classNames({ 'dimmed': nav.flightMode !== 'CRUISE' })}>Cruise</span>
-              /
-              <span className={classNames({ 'dimmed': nav.flightMode !== 'DRIFT' })}>Drift</span>
-              /
-              <span className={classNames({ 'dimmed': nav.flightMode !== 'STEALTH' })}>Stealth</span>
-              /
-              <span className={classNames({ 'dimmed': nav.flightMode !== 'BURN' })}>Burn</span>
-            </div>
+            <span className={classNames({ 'dimmed': nav.flightMode !== 'CRUISE' })}>Cruise</span>
+            {' / '}
+            <span className={classNames({ 'dimmed': nav.flightMode !== 'DRIFT' })}>Drift</span>
+            {' / '}
+            <span className={classNames({ 'dimmed': nav.flightMode !== 'STEALTH' })}>Stealth</span>
+            {' / '}
+            <span className={classNames({ 'dimmed': nav.flightMode !== 'BURN' })}>Burn</span>
           </div>
         </div>
 
@@ -77,7 +76,9 @@ export const Ship = ({ ship }: ShipProps) => {
               Fuel
             </div>
             <div className="info-bit-content">
-              {fuel.current} / {fuel.capacity}
+              <Tooltip tooltipText="The current amount of fuel in the ship's tanks.">{fuel.current}</Tooltip>
+              {' / '}
+              <Tooltip tooltipText="The maximum amount of fuel the ship's tanks can hold.">{fuel.capacity}</Tooltip>
             </div>
           </div>
 
@@ -86,7 +87,10 @@ export const Ship = ({ ship }: ShipProps) => {
               Cargo
             </div>
             <div className="info-bit-content">
-              {cargo.units} / {cargo.capacity}
+              <Tooltip tooltipText="The number of items currently stored in the cargo hold.">{cargo.units}</Tooltip>
+              {' / '}
+              <Tooltip
+                tooltipText="The max number of items that can be stored in the cargo hold.">{cargo.capacity}</Tooltip>
             </div>
           </div>
 
@@ -95,9 +99,23 @@ export const Ship = ({ ship }: ShipProps) => {
               Crew
             </div>
             <div className="info-bit-content">
-              <span className="dimmed">[{crew.required} - </span>
-              {crew.current}
-              <span className="dimmed"> - {crew.capacity}]</span>
+              <span className="dimmed">
+                [
+                <Tooltip tooltipText="The minimum number of crew members required to maintain the ship.">
+                  {crew.required}
+                </Tooltip>
+              </span>
+              <span className="dimmed">{' - '}</span>
+              <Tooltip tooltipText="The current number of crew members on the ship.">
+                {crew.current}
+              </Tooltip>
+              <span className="dimmed">{' - '}</span>
+              <span className="dimmed">
+                <Tooltip tooltipText="The maximum number of crew members the ship can support.">
+                  {crew.capacity}
+                </Tooltip>
+                ]
+              </span>
             </div>
           </div>
 
