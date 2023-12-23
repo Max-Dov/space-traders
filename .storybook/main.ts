89c3be6c8634024737bd6c880e501e461fb3da1e
Max-Dov <maksimdovydenko@protonmail.com>
2023-12-23 18:12:04 +0100

    threejs-and-storybook: fix storybook configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,7 +20,8 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack'
+    '@storybook/addon-styling-webpack',
+    '@storybook/preset-scss'
   ],
   framework: {
     name: '@storybook/react-webpack5',
